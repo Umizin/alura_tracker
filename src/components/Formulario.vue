ed9d875acfee8b1a82794a4d670b37b0e5b65cf8
paulo.goncalves <paulo.goncalves@libresolucoes.com.br>
2025-07-21 17:06:35 -0300

    Particinalizei o código para que seja mais fácil a vizualização

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -2,10 +2,10 @@
     <div class="box">
         <div class="columns">
             <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
-                <input type="text" class="input" placeholder="Qual tarefa você deseja incluir?">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja incluir?" v-model="descricao">
             </div>
             <div class="column">
-                <Temporizador />
+                <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
             </div>
         </div>       
     </div>
@@ -20,6 +20,18 @@ export default defineComponent({
     name: 'Formulario',
     components: {
         Temporizador
+    },
+    data() {
+        return {
+            descricao: ''
+        }
+    },
+    methods:{
+        finalizarTarefa(tempoDecorrido: number) : void{
+            console.log(`Tarefa finalizada com ${tempoDecorrido} segundos.`);
+            console.log(`Descrição da tarefa: ${this.descricao}`);
+            this.descricao = '';
+        }
     }
 });
 </script>
--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,46 +1,44 @@
 <template>
-    <div class="is-flex is-align-items-center is-justify-content-space-between">
-    <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
-    <button class="button" @click="iniciar">
-        <span class="icon">
-            <i class="fas fa-play"></i>
-        </span>
-        <span>play</span>
-    </button>
-    <button class="button" @click="finalizar">
-        <span class="icon">
-            <i class="fas fa-stop"></i>
-        </span>
-        <span>stop</span>
-    </button>
-    </div>
-</template>
+    <section class="is-flex is-align-items-center is-justify-content-space-between">
+      <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
+      <Botao @clicado="iniciar" icone="fas fa-play" texto="play" :desabilitado="cronometroRodando" />
+      <Botao @clicado="finalizar" icone="fas fa-stop" texto="stop" :desabilitado="!cronometroRodando" />
+    </section>
+  </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Cronometro from './Cronometro.vue';
+import Botao from './Botao.vue';
 
 export default defineComponent({
     // eslint-disable-next-line vue/multi-word-component-names
     name: 'Temporizador',
+    emits: ['aoTemporizadorFinalizado'],
     components: {
         Cronometro
+        , Botao
     },
     data( ){
         return{
             tempoEmSegundos: 0,
-            cronometro: 0
+            cronometro: 0,
+            cronometroRodando: false
         }
     },
 
     methods:{
         iniciar(){
+            this.cronometroRodando = true;
             this.cronometro = setInterval(()=>{
                 this.tempoEmSegundos++;
             },1000)
         },
         finalizar(){
+            this.cronometroRodando = false;
             clearInterval(this.cronometro);
+            this.$emit('aoTemporizadorFinalizado', this.tempoEmSegundos);
+            this.tempoEmSegundos = 0
         },
     }
 });
